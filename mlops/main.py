bef990639544f650c174caadaf467e626fe761c0
m-somov <matvey.somov.3141@gmail.com>
2024-10-22 22:26:25 +0500

    tooling: добавлен линтинг, тайпчекер и pre-commit хуки

--- mlops/main.py
+++ mlops/main.py
@@ -1,2 +1,6 @@
 def sumTwoInt(a: int, b: int) -> int:
-  return a + b
+    return a + b
+
+
+def printLongLine():
+    print("______________________Это очень длинная строка, длина которой составляет 98 символов")
